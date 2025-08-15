a96af0f8f23845a5e8f01157c9dbf4407204fc5b
Tyler Combs <tyler.combs@gmail.com>
2025-08-15 03:11:52 -0500

    new file:   amplify/storage/resources.ts 	modified:   package-lock.json 	modified:   package.json 	modified:   src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { Amplify } from "aws-amplify";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import outputs from "../amplify_outputs.json";
+// Make sure Main.tsx or Main.jsx exists in ./components
+// Ensure the file exists at ./components/Main.tsx or ./components/Main.jsx and the import matches the file name exactly
 import Main from "./components/Main";
 import "./index.css";
 
